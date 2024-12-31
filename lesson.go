85adf7cbaddf074b1c4ceda3f8fd95325e6820a2
radiol <radiol@github.com>
2024-12-31 23:29:10 +0900

    Add goroutine to calculate running sum of slice elements

--- lesson.go
+++ lesson.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func goroutine(s []int, c chan int) {
+	defer close(c)
+	sum := 0
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+}
 
 func main() {
-  fmt.Println("Hello, World!")
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int, len(s))
+	go goroutine(s, c)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
